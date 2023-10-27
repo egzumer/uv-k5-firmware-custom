7ed842c198bca5a8d98cbd5c900c0c2d4a47a8fd
Krzysiek Egzmont <egzumer@gmail.com>
2023-10-27 19:00:42 +0200

    Comments, refactor

--- ui/main.c
+++ ui/main.c
@@ -40,7 +40,7 @@ center_line_t center_line = CENTER_LINE_NONE;
 
 // ***************************************************************************
 
-static void DrawBars(uint8_t *p, unsigned int level)
+static void DrawSmallAntennaAndBars(uint8_t *p, unsigned int level)
 {
 	if(level>6)
 		level = 6;
@@ -152,12 +152,16 @@ static void DisplayRSSIBar(const int16_t rssi, const bool now)
 		if (now)
 			memset(p_line, 0, LCD_WIDTH);
 
-		const int16_t  s0_dBm       = -121 - 6;                  // S1 .. base level
-		const uint16_t s1_rssi = gEEPROM_RSSI_CALIB[gRxVfo->Band][0];
-		const uint16_t s9_rssi = gEEPROM_RSSI_CALIB[gRxVfo->Band][3];
-		const int16_t  rssi_dBm     = s0_dBm + 6 + (rssi - s1_rssi) * 8 * 6 / (s9_rssi - s1_rssi); //(rssi / 2) - 160;
+		const int16_t  s0_dBm  = -121 - 6;                  // S0 .. base level
+		const uint16_t s1_rssi = gEEPROM_RSSI_CALIB[gRxVfo->Band][0]; // min callibration point
+		const uint16_t s9_rssi = gEEPROM_RSSI_CALIB[gRxVfo->Band][3]; // max callibration point
 
-		const uint8_t s_level = MIN(MAX((rssi_dBm - s0_dBm) / 6, 0), 9);
+		// it takes min and max rssi calibration points and assumes it is for s1 and s9 
+		// (s1 = -121dBm and s9 = -73dBm) according to this https://en.wikipedia.org/wiki/S_meter
+		// then calculates the scale to match it and from that we have value in dBm
+		const int16_t rssi_dBm = s0_dBm + 6 + (rssi - s1_rssi) * 8 * 6 / (s9_rssi - s1_rssi);
+
+		const uint8_t s_level = MIN(MAX((rssi_dBm - s0_dBm) / 6, 0), 9); // S0 - S9
 		uint8_t overS9dBm = MIN(MAX(73 + rssi_dBm, 0), 99);
 		uint8_t overS9Bars = MIN(overS9dBm/10, 4);
 		
@@ -198,7 +202,7 @@ static void DisplayRSSIBar(const int16_t rssi, const bool now)
 	uint8_t *pLine = (gEeprom.RX_VFO == 0)? gFrameBuffer[2] : gFrameBuffer[6];
 	if (now)
 		memset(pLine, 0, 23);
-	DrawBars(pLine, Level);
+	DrawSmallAntennaAndBars(pLine, Level);
 #endif
 
 	if (now)
@@ -552,7 +556,7 @@ void UI_DisplayMain(void)
 				#endif
 			}
 			if(Level)
-				DrawBars(p_line1 + LCD_WIDTH, Level);
+				DrawSmallAntennaAndBars(p_line1 + LCD_WIDTH, Level);
 		}
 
 		// ************
