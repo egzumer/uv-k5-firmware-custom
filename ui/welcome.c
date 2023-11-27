9caf8a9d82eb41e1a3cb7bab280217da0948b0f1
Joaquim José Almeida Pereira <joaquim.org@gmail.com>
2023-11-27 21:26:08 +0000

    welcome

--- ui/welcome.c
+++ ui/welcome.c
@@ -77,7 +77,8 @@ void UI_DisplayWelcome(void)
 
 		UI_PrintString(WelcomeString0, 0, 127, 0, 10);
 		UI_PrintString(WelcomeString1, 0, 127, 2, 10);
-		UI_PrintStringSmallBold(Version, 0, 127, 5);
+		UI_PrintStringSmallBold(Version, 0, 127, 4);
+		UI_PrintStringSmall(__DATE__, 0, 127, 6);
 
 		ST7565_BlitStatusLine();  // blank status line
 		ST7565_BlitFullScreen();
