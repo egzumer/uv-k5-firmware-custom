dc1808b1fe379e776522ddaea9194a7d4dd863eb
Krzysiek Egzmont <egzumer@gmail.com>
2024-01-29 16:50:58 +0100

    FIX #444: build error when ENABLE_DTMF_CALLING disabled

--- app/menu.c
+++ app/menu.c
@@ -1404,7 +1404,12 @@ static void MENU_Key_MENU(const bool bKeyPressed, const bool bKeyHeld)
 			if (UI_MENU_GetCurrentMenuId() != MENU_SCR)
 				gAnotherVoiceID = MenuList[gMenuCursor].voice_id;
 		#endif
-        if (UI_MENU_GetCurrentMenuId() == MENU_ANI_ID || UI_MENU_GetCurrentMenuId() == MENU_UPCODE|| UI_MENU_GetCurrentMenuId() == MENU_DWCODE)
+        if (UI_MENU_GetCurrentMenuId() == MENU_UPCODE 
+			|| UI_MENU_GetCurrentMenuId() == MENU_DWCODE 
+#ifdef ENABLE_DTMF_CALLING 
+			|| UI_MENU_GetCurrentMenuId() == MENU_ANI_ID
+#endif
+			)
             return;
 		#if 1
 			if (UI_MENU_GetCurrentMenuId() == MENU_DEL_CH || UI_MENU_GetCurrentMenuId() == MENU_MEM_NAME)
