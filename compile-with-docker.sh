6d92e15d5a18d88b0b8873c6d1c19651485623cd
Lars Lehtonen <lars.lehtonen@gmail.com>
2024-05-24 12:52:34 -0700

    Dockerfile.egzumer

--- compile-with-docker.sh
+++ compile-with-docker.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
-docker build -t uvk5 .
-docker run --rm -v ${PWD}/compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app && make && cp firmware* compiled-firmware/"
+#!/bin/env sh
+mkdir -p compiled-firmware
+docker build -t egzumer -f Dockerfile.egzumer .
+docker run --rm --user `id -u`:`id -g` -v `pwd`:/app egzumer \
+/bin/bash -c 'make && mv firmware firmware.bin firmware.packed.bin compiled-firmware && chmod -R 644 compiled-firmware/*'
